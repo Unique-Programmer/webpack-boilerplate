a62d8821595eeb2f9e0a2af0ce12e3ea41ce2863
vberyoza <v.beryoza@orbita.center>
2024-08-05 03:29:22 +0500

    feat: config decomposition

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,29 @@
+import webpack, { Configuration } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from './types';
+
+export function buildPlugins({ mode, paths }: BuildOptions): Configuration['plugins'] {
+    const isDev = mode === 'development';
+    const isProd = mode === 'production';
+
+    const plugins: Configuration['plugins'] = [
+        new HtmlWebpackPlugin({ template: paths.html }),
+    ];
+
+    if (isDev) {
+        plugins.push(new webpack.ProgressPlugin());
+    }
+
+    if (isProd) {
+        plugins.push(
+            // MiniCssExtractPlugin for extracting css in own files (!recommended)
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css',
+            })
+        );
+    }
+
+    return plugins;
+}
---
+++ config/build/buildWebpack.ts
@@ -0,0 +1,31 @@
+import webpack from 'webpack';
+import { buildDevServer } from './buildDevServer';
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugins';
+import { buildResolvers } from './buildResolvers';
+import { BuildOptions } from './types';
+
+export function buildWebpack(options: BuildOptions): webpack.Configuration {
+    const { mode, paths } = options;
+    const isDev = mode === 'development';
+
+    return {
+        mode: mode ?? 'development',
+        // entry can be array of entry points
+        entry: paths.entry,
+        output: {
+            path: paths.output,
+            // setting file name with addition parts from content hash
+            filename: '[name].[contenthash].js',
+            // auto build clean previous for new
+            clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(),
+        devtool: isDev && 'inline-source-map',
+        devServer: isDev ? buildDevServer(options) : undefined,
+    }
+}
